65bc3aa0cdc388642836115572951c3004896b6c
Renato Alves <reaaldev@gmail.com>
2021-08-07 12:17:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Clima ☁
+
+A weather app able to find out the live weather data in the current location of the device as well as the weather for any city you can think of!
+
+
+## What you will learn
+
+- Perform asynchronous tasks.
+- Use of async and await.
+- Futures.
+- Network with the Dart http package.
+- Get data from the internet through API.
+- Parse JSONs using the Dart convert package.
+- Pass data forwards and backwards between screens using the Navigator.
+- Handle exceptions in Dart using try/catch/throw.
+- Lifecycle of Stateful Widgets.
+- Use the Geolocator package to get live location data for both iOS and Android.
+- Use the TextField Widget to take user input.
+
+
+>This was a companion project to The App Brewery's Complete Flutter Development Bootcamp from Udemy that I finished in 2020. Inspired by the designs made by [Olia Gozha](https://dribbble.com/shots/4663154-)
---
+++ lib/services/weather.dart
@@ -0,0 +1,59 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+import 'package:flutter/foundation.dart';
+
+const apiKey = '2c0aa11b5d6c3e06f28b5adb91076146';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+    debugPrint('$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  String getWeatherIcon(int condition) {
+    if (condition < 300) {
+      return '🌩';
+    } else if (condition < 400) {
+      return '🌧';
+    } else if (condition < 600) {
+      return '☔️';
+    } else if (condition < 700) {
+      return '☃️';
+    } else if (condition < 800) {
+      return '🌫';
+    } else if (condition == 800) {
+      return '☀️';
+    } else if (condition <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
